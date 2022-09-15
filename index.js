b7dec78b9a1b82e6cf62c887060a34316c0737aa
arifur-rahman1 <aarifurrahman93@gmail.com>
2022-09-15 19:51:28 +0600

    update item done

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 var cors = require('cors')
 const port = process.env.PORT || 5000
@@ -15,10 +15,10 @@ app.use(express.json())
 
 const uri = "mongodb+srv://admin1:nTLJ0s8GdmJ6levn@cluster0.wa54gsl.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-async function run(){
-    try{
-       await client.connect();
-       const itemCollection = client.db('Inventory-management').collection('item');
+async function run() {
+    try {
+        await client.connect();
+        const itemCollection = client.db('Inventory-management').collection('item');
 
         //Post 
         app.post('/item', async (req, res) => {
@@ -29,17 +29,41 @@ async function run(){
 
         // get items
         app.get('/item', async (req, res) => {
-            const category=req.query.category;
-            const query = {category:category};
+            const category = req.query.category;
+            const query = { category: category };
             const cursor = itemCollection.find(query);
             const items = await cursor.toArray();
             res.send(items);
         })
 
+        //delete 
 
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
+        });
+
+        //update Item
+        app.put('/item/:id', async (req, res) => {
+            const id = req.params.id;
+            const updatedQuantity = req.body;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set:{
+                    displayName:updatedQuantity.displayName,
+                    description:updatedQuantity.description,
+                    price:updatedQuantity.price,
+                }
+            }
+            const result = await itemCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        })
 
     }
-    finally{
+    finally {
 
     }
 }
@@ -49,9 +73,9 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-  res.send('Inventroy managment')
+    res.send('Inventroy managment')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
